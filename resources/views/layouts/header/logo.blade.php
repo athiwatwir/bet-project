b4affd2697f7cebf45a64dba26463ef688cbebaa
Athiwat Wiroonphattanadate <athiwat.wir@gmail.com>
2021-10-26 16:53:12 +0700

    update logo

--- resources/views/layouts/header/logo.blade.php
+++ resources/views/layouts/header/logo.blade.php
@@ -1,5 +1,5 @@
 <div class="d-inline-block float-start">
     <a class="navbar-brand" href="{{ url('/') }}">
-        <img src="{{ asset('img/logo/logo-w.png') }}" width="110" height="50" alt="...">
+        <img src="{{ asset('img/logo/logo-w.png') }}" width="160" height="44" alt="...">
     </a>
 </div>
